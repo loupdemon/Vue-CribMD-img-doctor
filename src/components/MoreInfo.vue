4a12f28554fd826c51db6d2d69975d7e538b0163
Gromov Danil <105980448+Xenob1na@users.noreply.github.com>
2023-12-31 15:23:49 +0700

    Update

--- src/components/MoreInfo.vue
+++ src/components/MoreInfo.vue
@@ -6,19 +6,19 @@
             <p class="text-[#1971F5] text-[16px] leading-7 text-center tracking-[0.8px] italic">N.B: Refer a friend and stand a chance to win a free doctor consultation.</p>
         </div>
         <div  class="lg3:mx-auto lg3:max-w-[500px]">
-            <BoxLeft img="/img/f1.png" title="Plan Type 1" ul1="50% Off for the first month" ul2="80% Off for 6 months" ul3="Terms and Conditions apply" btns="Get discount on plan"> 
+            <BoxLeft img="./img/f1.png" title="Plan Type 1" ul1="50% Off for the first month" ul2="80% Off for 6 months" ul3="Terms and Conditions apply" btns="Get discount on plan"> 
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Odio pharetra nec aliquam facilisi phasellus. Eleifend.
             </BoxLeft>
-            <BoxRight img="/img/f2.png" title="Plan Type 2" ul1="25% Off for the first month" ul2="90% Off for 6 months" ul3="Terms and Conditions apply" btns="Get discount on plan">
+            <BoxRight img="./img/f2.png" title="Plan Type 2" ul1="25% Off for the first month" ul2="90% Off for 6 months" ul3="Terms and Conditions apply" btns="Get discount on plan">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Odio pharetra nec aliquam facilisi phasellus. Eleifend.
             </BoxRight>
-            <BoxLeft img="/img/f3.png" title="Plan Type 3" ul1="15% Off for the first month on Black Friday" ul2="20% Off for the first month on Cyber Monday" ul3="90% Off for 6 months" btns="Get discount on plan"> 
+            <BoxLeft img="./img/f3.png" title="Plan Type 3" ul1="15% Off for the first month on Black Friday" ul2="20% Off for the first month on Cyber Monday" ul3="90% Off for 6 months" btns="Get discount on plan"> 
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Odio pharetra nec aliquam facilisi phasellus. Eleifend.
             </BoxLeft>
-            <BoxRight img="/img/f4.png" title="Plan Type 4" ul1="25% Off for the first month" ul2="90% Off for 6 months" ul3="Terms and Conditions apply" btns="Contact Support">
+            <BoxRight img="./img/f4.png" title="Plan Type 4" ul1="25% Off for the first month" ul2="90% Off for 6 months" ul3="Terms and Conditions apply" btns="Contact Support">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Odio pharetra nec aliquam facilisi phasellus. Eleifend.
             </BoxRight>
-            <BoxLeft img="/img/f5.png" title="Plan Type 5" ul1="90% Off on 26th" ul2="40% Off on Cyber Monday" ul3="30% off Everyday" btns="Contact Support"> 
+            <BoxLeft img="./img/f5.png" title="Plan Type 5" ul1="90% Off on 26th" ul2="40% Off on Cyber Monday" ul3="30% off Everyday" btns="Contact Support"> 
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Odio pharetra nec aliquam facilisi phasellus. Eleifend.
             </BoxLeft>
         </div>
